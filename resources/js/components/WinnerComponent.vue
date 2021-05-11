d4abd89037ac3590b56560f3fa55f72a21e122e0
idegart <degtyarev_artem@mail.ru>
2021-05-11 18:25:39 +0300

    Fix animation

--- resources/js/components/WinnerComponent.vue
+++ resources/js/components/WinnerComponent.vue
@@ -3,33 +3,32 @@
     <form @submit.prevent autocomplete="off">
       <h1 class="h3 fw-normal">Розыгрыш</h1>
 
-      <div v-if="loading" class="alert alert-info" role="alert">
+      <div class="d-flex justify-content-around winner-code">
+        <div v-for="n in 5" class="winner-code__char">
+          <transition name="fade" mode="out-in">
+            <span v-if="showCode(n - 1)" v-text="showCode(n - 1)"></span>
+          </transition>
+        </div>
+      </div>
+
+      <div v-if="loading" class="alert alert-info mt-3" role="alert">
         Поиск победителя
       </div>
 
-      <div v-if="!codeParts" class="input-group input-group-lg justify-content-center mt-3">
-        <input v-for="n in 5" aria-label="item"
-               class="form-control text-center"
-               style="max-width: 50px" readonly>
-      </div>
-
-      <div v-else class="input-group input-group-lg justify-content-center mt-3">
-        <input v-for="n in codeParts"
-               :value="n" aria-label="item"
-               class="form-control text-center"
-               style="max-width: 50px" readonly>
-      </div>
-
-      <div v-if="winner" class="mt-3">
-        <h1>Победитель!</h1>
-        <h2 v-text="winner.name"></h2>
-      </div>
+      <transition name="fade" mode="out-in">
+        <div v-if="winnerName" class="mt-3">
+          <h1>Победитель!</h1>
+          <h2 v-text="winnerName"></h2>
+        </div>
+      </transition>
     </form>
   </main>
 </template>
 
 <script>
 import axios from "@plugin/axios";
+
+const duration = 600;
 
 export default {
   name: "WinnerComponent",
@@ -38,19 +37,32 @@ export default {
     winner: null,
     codeParts: null,
     loading: false,
+    winnerName: null,
   }),
 
   methods: {
     loadWinner() {
       this.loading = true
       this.winner = null
+      this.winnerName = null
+      this.codeParts = []
+
       axios.post('api/winner')
           .then(({data}) => {
             if (data.success === false) {
               alert("Победитель не найден")
             }
             this.winner = data.data
-            this.codeParts = this.winner.code.code.split('')
+            let parts = this.winner.code.code.split('')
+            parts.forEach(part => {
+              setTimeout(() => {
+                this.codeParts.push(part)
+              }, part * duration)
+            })
+
+            setTimeout(() => {
+              this.winnerName = this.winner.name
+            }, (parts.length + 1) * duration)
           })
           .catch(() => {
             alert("При поиске победителя возникла ошибка")
@@ -58,6 +70,14 @@ export default {
           .finally(() => {
             this.loading = false
           })
+    },
+
+    showCode (n) {
+      if (!this.winner) {
+        return null
+      }
+
+      return this.codeParts && this.codeParts[n] || null;
     },
 
     doCommand(e) {
@@ -79,5 +99,27 @@ export default {
 </script>
 
 <style scoped>
+.winner-code {
+  border-radius: .3rem;
+  background-color: #e9ecef;
+  border: 1px solid #ced4da;
+}
+.winner-code > div {
+  border-right: 1px solid #ced4da;
+  padding: .5rem 1rem;
+  font-size: 1.25rem;
+  height: 2.75rem;
+  width: 3rem;
+}
+.winner-code > div:last-child {
+  border-right: none;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity;
+  transition-duration: .5s
+}
 
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
